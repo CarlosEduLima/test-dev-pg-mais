91e2a8f1d2fe110764711d31f4f9bde7413b68af
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-04-01 21:42:08 -0300

    create default http responses

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 app.use(cors())
 app.use(bodyParser.json())
-Routes(app)
 app.use(express.urlencoded({ extended: false }))
 
 const PORT = process.env.PORT || 3001
