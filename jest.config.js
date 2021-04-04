438e647b72d014b2c19fa6c0448eea24e8746cd8
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-04-04 20:21:22 -0300

    Add setup file

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  setupFilesAfterEnv: ['./setup.js']
 }
