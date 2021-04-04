42c33fbf71b69a16c26e461b1176482ae8aea349
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-04-04 19:51:33 -0300

    Change valiaded to success

--- src/domain/message-cases/blacklist-case.spec.js
+++ src/domain/message-cases/blacklist-case.spec.js
@@ -3,7 +3,7 @@ const { phoneNumbers } = require('../../../mocks/phone-numbers-array')
 describe('Blacklist case', () => {
   test('should return 500 and false validated if no array is provided', async () => {
     const validPhoneNumbers = await BlacklistCase()
-    expect(validPhoneNumbers.validated).toBe(false)
+    expect(validPhoneNumbers.success).toBe(false)
     expect(validPhoneNumbers.httpResponse.statusCode).toBe(500)
     expect(validPhoneNumbers.httpResponse.body).toEqual({ error: 'No phone numbers array provided' })
   })
