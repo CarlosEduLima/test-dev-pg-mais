05db379e801eaf214a736b49b9197e0a16e51883
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-04-04 20:19:15 -0300

    Add more message objects

--- mocks/phone-numbers-array.js
+++ mocks/phone-numbers-array.js
@@ -4,20 +4,37 @@ const phoneNumbers = [
     DDD: '12',
     CELULAR: '996958849',
     OPERADORA: 'NEXTEL',
+    HORARIO_ENVIO: '21:24:03',
     MENSAGEM: 'sapien sapien non mi integer ac neque duis bibendum'
   },
   {
+    IDMENSAGEM: 'b7e2af69-ce52-4812-adf1-395c8875ad30',
+    DDD: '46',
+    CELULAR: '950816645',
+    OPERADORA: 'CLARO',
+    HORARIO_ENVIO: '19:05:21',
+    MENSAGEM: 'justo lacinia eget tincidunt eget'
+  },
+  {
     IDMENSAGEM: 'e7b87f43-9aa8-414b-9cec-f28e653ac25e',
-    DDD: '55',
-    CELULAR: '929527353',
+    DDD: '34',
+    CELULAR: '990171682',
     OPERADORA: 'VIVO',
     HORARIO_ENVIO: '18:35:20',
     MENSAGEM: 'dui luctus rutrum nulla tellus in sagittis dui'
   },
   {
+    DDD: '43',
+    CELULAR: '940513739',
+    OPERADORA: 'NEXTEL',
+    HORARIO_ENVIO: '14:54:16',
+    MENSAGEM: 'nibh fusce lacus purus aliquet at feugiat'
+  },
+  {
     IDMENSAGEM: 'd81b2696-8b62-4b8b-af82-586ce0875ebc',
     DDD: '21',
     CELULAR: '983522711',
+    OPERADORA: 'TIM',
     HORARIO_ENVIO: '16:42:48',
     MENSAGEM: 'sit amet eros suspendisse accumsan tortor quis turpis sed ante'
   }
